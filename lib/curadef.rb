c936b0fb617445a01cb44cc41cd9d76a2d9425a1
Joseph Sungpil Choi <chousemath@gmail.com>
2017-10-12 15:49:45 +0900

    added more tests, using the splat operator

--- lib/curadef.rb
+++ lib/curadef.rb
@@ -18,7 +18,7 @@ module Curadef
     INFILL_PATTERNS.key?(pattern) ? INFILL_PATTERNS[pattern] : 1.0
   end
 
-  def self.infill_line_distance(infill_sparse_density, infill_line_width, infill_pattern)
+  def self.infill_line_distance(value, *args)
     # "label": "Infill Line Distance",
     # "description": "Distance between the printed infill lines. This setting
     #                is calculated by the infill density and the infill line
@@ -37,15 +37,16 @@ module Curadef
     #          1))))",
     # "limit_to_extruder": "infill_extruder_nr",
     # "settable_per_mesh": true
-    return 0.0 if infill_sparse_density == 0.0
-    value = (infill_line_width * 100.0) / infill_sparse_density * infill_multiplier(infill_pattern)
-    # truncate that value to 3 decimal points
-    truncated_value = truncate_num(value)
-    puts "At or below minimum value (#{infill_line_width})" if truncated_value < infill_line_width
-    [0, truncated_value].max
+    unless value
+      infill_sparse_density, infill_line_width, infill_pattern = args
+      return 0.0 if infill_sparse_density == 0.0
+      value = truncate_num((infill_line_width * 100.0) / infill_sparse_density * infill_multiplier(infill_pattern))
+      puts "At or below minimum value (#{infill_line_width})" if value < infill_line_width
+    end
+    [0, value].max
   end
 
-  def self.wall_line_count(magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+  def self.wall_line_count(value, *args)
     # "label": "Wall Line Count",
     # "description": "The number of walls. When calculated by the wall thickness, this value is rounded to a whole number.",
     # "default_value": 2,
@@ -56,17 +57,38 @@ module Curadef
     # "value": "1 if magic_spiralize else max(1, round((wall_thickness - wall_line_width_0) / wall_line_width_x) + 1) if wall_thickness != 0 else 0",
     # "limit_to_extruder": "wall_x_extruder_nr",
     # "settable_per_mesh": true
-    return 1 if magic_spiralize
-    return 0 if wall_thickness <= 0.0
-    if wall_line_width_x <= 0
-      puts "Invalid value for wall_line_width_x, expected: greater than 0, "\
-           "got: #{wall_line_width_x}"
-      return 0
+    unless value
+      magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x = args
+      return 1 if magic_spiralize
+      return 0 if wall_thickness <= 0.0
+      if wall_line_width_x <= 0
+        puts "Invalid value for wall_line_width_x, expected: greater than 0, "\
+             "got: #{wall_line_width_x}"
+        return 0
+      end
+      value = truncate_num(((wall_thickness - wall_line_width_0).round / wall_line_width_x) + 1)
+      puts "At or below minimum value (1)" if value <= 1
+      puts "At or above maximum value (10)" if value >= 10
     end
-    value = ((wall_thickness - wall_line_width_0).round / wall_line_width_x) + 1
-    puts "At or below minimum value (1)" if value <= 1
-    puts "At or above maximum value (10)" if value >= 10
-    [1, truncate_num(value)].max
+    [1, value].max
+  end
+
+  def self.wall_0_wipe_dist(value, *args)
+    # "label": "Outer Wall Wipe Distance",
+    # "description": "Distance of a travel move inserted after the outer wall, to hide the Z seam better.",
+    # "unit": "mm",
+    # "type": "float",
+    # "default_value": 0.2,
+    # "value": "machine_nozzle_size / 2",
+    # "minimum_value": "0",
+    # "maximum_value_warning": "machine_nozzle_size * 2",
+    # "limit_to_extruder": "wall_0_extruder_nr",
+    # "settable_per_mesh": true
+    unless value
+      machine_nozzle_size = args[0]
+      value = truncate_num(machine_nozzle_size / 2.0)
+    end
+    [0, value].max
   end
 
   def self.truncate_num(value)
--- spec/curadef_spec.rb
+++ spec/curadef_spec.rb
@@ -36,101 +36,143 @@ describe Curadef do
   end
 
   context 'infill_line_distance' do
+    it 'returns a value if given one' do
+      actual = Curadef.infill_line_distance(2)
+      expect(actual).to eq(2)
+    end
+    it 'returns a value if given one, regardless of the following values' do
+      infill_line_width = 123.321
+      infill_sparse_density = 75.432
+      infill_pattern = :cubic
+      actual = Curadef.infill_line_distance(2, infill_sparse_density, infill_line_width, infill_pattern)
+      expect(actual).to eq(2)
+    end
     it 'returns the correct infill_line_distance (trial 1)' do
       infill_line_width = 123.321
       infill_sparse_density = 75.432
       infill_pattern = :cubic
-      actual = Curadef.infill_line_distance(infill_sparse_density, infill_line_width, infill_pattern)
+      actual = Curadef.infill_line_distance(nil, infill_sparse_density, infill_line_width, infill_pattern)
       expect(actual).to eq(490.458)
     end
-
     it 'returns the correct infill_line_distance (trial 2)' do
       infill_line_width = 2.32155
       infill_sparse_density = 15.333
       infill_pattern = :cross
-      actual = Curadef.infill_line_distance(infill_sparse_density, infill_line_width, infill_pattern)
+      actual = Curadef.infill_line_distance(nil, infill_sparse_density, infill_line_width, infill_pattern)
       expect(actual).to eq(15.140)
     end
-
     it 'returns the minimum value (0) trial 1' do
       infill_line_width = -2.32155
       infill_sparse_density = 15.333
       infill_pattern = :cross
-      actual = Curadef.infill_line_distance(infill_sparse_density, infill_line_width, infill_pattern)
+      actual = Curadef.infill_line_distance(nil, infill_sparse_density, infill_line_width, infill_pattern)
       expect(actual).to eq(0)
     end
-
     it 'returns the minimum value (0) trial 2' do
       infill_line_width = 2.32155
       infill_sparse_density = -15.333
       infill_pattern = :cross
-      actual = Curadef.infill_line_distance(infill_sparse_density, infill_line_width, infill_pattern)
+      actual = Curadef.infill_line_distance(nil, infill_sparse_density, infill_line_width, infill_pattern)
       expect(actual).to eq(0)
     end
   end
 
   context 'wall_line_count' do
+    it 'should return a value if given one' do
+      actual = Curadef.wall_line_count(5)
+      expect(actual).to eq(5)
+    end
+    it 'should return a value if given one, regardless of following values' do
+      magic_spiralize = true
+      wall_thickness = 0.5
+      wall_line_width_0 = 0.3
+      wall_line_width_x = 0.7
+      actual = Curadef.wall_line_count(5, magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+      expect(actual).to eq(5)
+    end
     it 'should return 1 if magic_spiralize is set to true' do
       magic_spiralize = true
       wall_thickness = 0.5
       wall_line_width_0 = 0.3
       wall_line_width_x = 0.7
-      actual = Curadef.wall_line_count(magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+      actual = Curadef.wall_line_count(nil, magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
       expect(actual).to eq(1)
     end
-
     it 'should return 0 if wall_thickness is 0' do
       magic_spiralize = false
       wall_thickness = 0.0
       wall_line_width_0 = 0.3
       wall_line_width_x = 0.7
-      actual = Curadef.wall_line_count(magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+      actual = Curadef.wall_line_count(nil, magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
       expect(actual).to eq(0)
     end
-
     it 'should return 0 if wall_thickness is negative' do
       magic_spiralize = false
       wall_thickness = -2.3
       wall_line_width_0 = 0.3
       wall_line_width_x = 0.7
-      actual = Curadef.wall_line_count(magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+      actual = Curadef.wall_line_count(nil, magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
       expect(actual).to eq(0)
     end
-
     it 'should return 0 if wall_line_width_x is 0' do
       magic_spiralize = false
       wall_thickness = 2.3
       wall_line_width_0 = 0.3
       wall_line_width_x = 0.0
-      actual = Curadef.wall_line_count(magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+      actual = Curadef.wall_line_count(nil, magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
       expect(actual).to eq(0)
     end
-
     it 'should return 0 if wall_line_width_x is negative' do
       magic_spiralize = false
       wall_thickness = 2.3
       wall_line_width_0 = 0.3
       wall_line_width_x = -0.7
-      actual = Curadef.wall_line_count(magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+      actual = Curadef.wall_line_count(nil, magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
       expect(actual).to eq(0)
     end
-
     it 'should return correct value trial 1' do
       magic_spiralize = false
       wall_thickness = 2.3
       wall_line_width_0 = 0.3
       wall_line_width_x = 0.7
-      actual = Curadef.wall_line_count(magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+      actual = Curadef.wall_line_count(nil, magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
       expect(actual).to eq(3.857)
     end
-
     it 'should return correct value trial 2' do
       magic_spiralize = false
       wall_thickness = 2.5
       wall_line_width_0 = 0.1
       wall_line_width_x = 0.9
-      actual = Curadef.wall_line_count(magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
+      actual = Curadef.wall_line_count(nil, magic_spiralize, wall_thickness, wall_line_width_0, wall_line_width_x)
       expect(actual).to eq(3.222)
+    end
+  end
+
+  context 'wall_0_wipe_dist' do
+    it 'should return a value if given one' do
+      machine_nozzle_size = -0.4
+      actual = Curadef.wall_0_wipe_dist(0.8)
+      expect(actual).to eq(0.8)
+    end
+    it 'should return a value if given one, regardless of following values' do
+      machine_nozzle_size = -0.4
+      actual = Curadef.wall_0_wipe_dist(0.8, 1, 2, 3)
+      expect(actual).to eq(0.8)
+    end
+    it 'should return minimum value if machine_nozzle_size is negative' do
+      machine_nozzle_size = -0.4
+      actual = Curadef.wall_0_wipe_dist(nil, machine_nozzle_size)
+      expect(actual).to eq(0)
+    end
+    it 'should return the correct value trial 1' do
+      machine_nozzle_size = 0.4
+      actual = Curadef.wall_0_wipe_dist(nil, machine_nozzle_size)
+      expect(actual).to eq(0.2)
+    end
+    it 'should return the correct value trial 2' do
+      machine_nozzle_size = 0.7
+      actual = Curadef.wall_0_wipe_dist(nil, machine_nozzle_size)
+      expect(actual).to eq(0.35)
     end
   end
 end
